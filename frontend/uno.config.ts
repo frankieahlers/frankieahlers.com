be7cb71bc21619a4309ed9e1469d8530b9d45837
meesh <meesh@found.com>
2024-01-20 15:01:04 -0500

    icon support

---
+++ frontend/uno.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, presetIcons, presetWind, presetTypography } from 'unocss';
+
+export default defineConfig({
+  presets: [
+    presetWind(),
+    presetIcons({
+      collections: {
+        uil: () => import('@iconify-json/uil/icons.json').then((l) => l.default),
+      },
+    }),
+    presetTypography(),
+  ],
+});
